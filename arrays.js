f0a2ea65607496e2fb9ca8472abce082112fd4fb
Christopher Michael Boujoukos <cboujoukos@gmail.com>
2017-10-18 13:50:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,3 +2,6 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 function addElementToBeginningOfArray(array, element) {
   [element, ... array]
 }
+function destructivelyAddElementToBeginningOfArray() {
+  array.unshift()
+}
